9f126ed8f8a6cd3b5d0513230fdd90a4d3f7f781
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-09-26 22:06:08 +0100

    emailservices

---
+++ src/nodemailer.ts
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer')
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'ajkshines001@gmail.com',
+        pass: 'Bobili001&23',
+    }
+})
+const sendMail = (Email:string, body: string) => { 
+    let mailOptions = {
+        from: 'ajkshines001@gmail.com',
+        to: Email,
+        subject: 'Successfully Signed up',
+        html: body
+    }
+    return transporter.sendMail(mailOptions, (err:any, data:any) => {
+        if (err) {
+            console.log('Error Occurred: ', err)
+        }
+        console.log('Email sent!!:' + data)
+    })
+}
+export default sendMail;
