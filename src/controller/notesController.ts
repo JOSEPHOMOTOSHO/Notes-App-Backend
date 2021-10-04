5367a2665f8b0545cbdb145c1b7d96997a4750a9
dandok <54486520+dandok@users.noreply.github.com>
2021-10-04 14:22:05 +0100

    Trash-Collaborators-Folders

--- src/controller/notesController.ts
+++ src/controller/notesController.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response, NextFunction } from "express";
-import Folder from "../model/folderModel";
-import Note from "../model/noteModel";
+import express, { Request, Response, NextFunction } from 'express';
+import Folder from '../model/folderModel';
+import Note from '../model/noteModel';
 
 //Function to create notes
 async function createNote(req: Request, res: Response, next: NextFunction) {
@@ -24,4 +24,20 @@ async function createNote(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export default createNote;
+async function getCollaborators(req: Request, res: Response) {
+  const { noteId } = req.params;
+  try {
+    const note = await Note.findById(noteId);
+
+    if (!note) return res.status(404).json({ error: 'Note does not exist' });
+    return res.status(200).json({
+      collaborators: note.collaboratorId,
+    });
+  } catch (err: any) {
+    res.status(400).json({
+      error: err.message,
+    });
+  }
+}
+
+export { createNote, getCollaborators };
--- src/routes/noteRoute.ts
+++ src/routes/noteRoute.ts
@@ -1,10 +1,8 @@
-import express , { Router } from 'express';
-import createNote from '../controller/notesController';
+import express, { Router } from 'express';
+import { createNote, getCollaborators } from '../controller/notesController';
 
 const router = Router();
 
-
-
-router.post('/:folderId', createNote )
-
+router.post('/:folderId', createNote);
+router.get('/:noteId/collaborators', getCollaborators);
 export default router;
