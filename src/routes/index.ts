f0d84f14945cdeb4868a02df6f855f6e6d10cb50
Chiemerie Anaebo <31117935+chiemerie386@users.noreply.github.com>
2021-10-15 07:02:17 +0100

    updated passport login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,10 +32,21 @@ router.get(
 );
 
 router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/users/profile/',
-    failureRedirect: '/users/login',
-    failureFlash: true,
+//   passport.authenticate('local', {
+//     successRedirect: '/users/profile/',
+//     failureRedirect: '/users/login',
+//     failureFlash: true,
+//   })(req, res, next);
+    passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
   })(req, res, next);
 });
 
