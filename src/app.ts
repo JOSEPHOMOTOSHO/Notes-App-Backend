c94d3fb928e4e115ab2807aba7710ff094c0fde6
Decagon <decagon@Nuel.local>
2021-11-03 08:26:55 +0100

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000", // <-- location of the react app were connecting to
+    origin: "*" , // <-- location of the react app were connecting to
     credentials: true,
   })
 );
