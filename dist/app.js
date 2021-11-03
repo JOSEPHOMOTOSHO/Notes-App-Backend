2f725105d1fbad0cbfb6b75a7615ac6aa704d171
Decagon <decagon@Nuel.local>
2021-11-03 09:04:28 +0100

    fixed cors issue

--- dist/app.js
+++ dist/app.js
@@ -30,7 +30,7 @@ app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)({
-    origin: "*",
+    origin: "http://localhost:3000",
     credentials: true,
 }));
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "*" , // <-- location of the react app were connecting to
+    origin: "http://localhost:3000", // <-- location of the react app were connecting to
     credentials: true,
   })
 );
