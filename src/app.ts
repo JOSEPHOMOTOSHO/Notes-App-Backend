3b32e90d32a586a781189d430342d749fb58a862
Decagon <decagon@Nuel.local>
2021-11-03 10:46:10 +0100

    fixed cors error

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,10 @@ app.use(
     credentials: true,
   })
 );
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin','*')
+  next()
+})
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
